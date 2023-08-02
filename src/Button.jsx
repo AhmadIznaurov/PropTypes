14af9f097188881ff051fc7be66c76f200bedcbe
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-08-02 16:01:27 +0300

    second

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const Button = (props) => {
   return (
     <button className={props.size} onClick={props.onClick}>
-      {props.text}
+      {props.children}
     </button>
   )
 }
 
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  onClick: PropTypes.func
+}
+
+Button.defaultProps = {
+  size: 'medium',
+  text: 'haha'
+}
